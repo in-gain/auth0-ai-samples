42645a23a60e6f31c12e007446393d26f094a885
Deepu <d4udts@gmail.com>
2025-01-22 12:50:46 +0100

    update fga scripts

--- authorization-for-rag/langgraph-agentic-js/scripts/fga-init.ts
+++ authorization-for-rag/langgraph-agentic-js/scripts/fga-init.ts
@@ -12,13 +12,13 @@ import { CredentialsMethod, OpenFgaClient } from "@openfga/sdk";
  */
 async function main() {
   const fgaClient = new OpenFgaClient({
-    apiUrl: process.env.FGA_API_URL!,
+    apiUrl: process.env.FGA_API_URL || "https://api.us1.fga.dev",
     storeId: process.env.FGA_STORE_ID!,
     credentials: {
       method: CredentialsMethod.ClientCredentials,
       config: {
-        apiTokenIssuer: process.env.FGA_API_TOKEN_ISSUER!,
-        apiAudience: process.env.FGA_API_AUDIENCE!,
+        apiTokenIssuer: process.env.FGA_API_TOKEN_ISSUER || "auth.fga.dev",
+        apiAudience: process.env.FGA_API_AUDIENCE || "https://api.us1.fga.dev/",
         clientId: process.env.FGA_CLIENT_ID!,
         clientSecret: process.env.FGA_CLIENT_SECRET!,
       },
