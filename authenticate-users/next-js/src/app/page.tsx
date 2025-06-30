8f885818b9907354817e8715e868dca0e591f777
Deepu K Sasidharan <d4udts@gmail.com>
2025-06-30 16:14:31 +0200

    feat: update next-js starter to assistant0

--- authenticate-users/next-js/src/app/page.tsx
+++ authenticate-users/next-js/src/app/page.tsx
@@ -1,36 +1,54 @@
-"use client";
+import { LogIn, UserPlus } from 'lucide-react';
+import { ChatWindow } from '@/components/ChatWindow';
+import { GuideInfoBox } from '@/components/guide/GuideInfoBox';
+import { Button } from '@/components/ui/button';
 
-import { useUser } from "@auth0/nextjs-auth0";
+export default async function Home() {
+  const InfoCard = (
+    <GuideInfoBox>
+      <ul>
+        <li className="text-l">
+          🤝
+          <span className="ml-2">
+            This template showcases a simple chatbot using Vercel's{' '}
+            <a className="text-blue-500" href="https://sdk.vercel.ai/docs" target="_blank">
+              AI SDK
+            </a>{' '}
+            in a{' '}
+            <a className="text-blue-500" href="https://nextjs.org/" target="_blank">
+              Next.js
+            </a>{' '}
+            project.
+          </span>
+        </li>
+        <li className="hidden text-l md:block">
+          💻
+          <span className="ml-2">
+            You can find the prompt and model logic for this use-case in <code>app/api/chat/route.ts</code>.
+          </span>
+        </li>
+        <li className="hidden text-l md:block">
+          🎨
+          <span className="ml-2">
+            The main frontend logic is found in <code>app/page.tsx</code>.
+          </span>
+        </li>
+        <li className="text-l">
+          👇
+          <span className="ml-2">
+            Try asking e.g. <code>What can you help me with?</code> below!
+          </span>
+        </li>
+      </ul>
+    </GuideInfoBox>
+  );
 
-export default function Page() {
-  // Extract the user object and loading state from Auth0.
-  const { user, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  // If no user, show sign-up and login buttons.
-  if (!user) {
-    return (
-      <main className="flex flex-col items-center justify-center h-screen">
-        <a href="/auth/login?screen_hint=signup">
-          <button>Sign up</button>
-        </a>
-        <a href="/auth/login">
-          <button>Log in</button>
-        </a>
-      </main>
-    );
-  }
-
-  // If user exists, show a welcome message and logout button.
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1>Welcome, {user.name}!</h1>
-      <p>
-        <a href="/auth/logout">
-          <button>Log out</button>
-        </a>
-      </p>
-    </main>
+    <ChatWindow
+      endpoint="api/chat"
+      emoji="🤖"
+      placeholder={`Hello, I'm your personal assistant. How can I help you today?`}
+      emptyStateComponent={InfoCard}
+    />
   );
 }
