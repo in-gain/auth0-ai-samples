081147e2cfba8946b40462678f6680e4d0eaf6d0
Deepu K Sasidharan <d4udts@gmail.com>
2025-06-30 16:51:31 +0200

    feat: genai-docs: call your own API

--- call-apis-on-users-behalf/your-api-next-js/src/app/api/chat/route.ts
+++ call-apis-on-users-behalf/your-api-next-js/src/app/api/chat/route.ts
@@ -1,22 +1,40 @@
-import { getUserInfoTool } from "@/lib/tools/user-info";
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { NextRequest } from 'next/server';
+import { streamText, Message, createDataStreamResponse, DataStreamWriter } from 'ai';
+import { openai } from '@ai-sdk/openai';
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+import { getUserInfoTool } from '@/lib/tools/user-info';
 
-  // streamText is used to run the request
-  const result = streamText({
-    messages,
-    model: openai("gpt-4o-mini"),
-    // Provides external tools the model can call.
-    // In this case, a User Info tool.
-    tools: { getUserInfoTool },
-    maxSteps: 2,
-    onError({ error }) {
-      console.error("streamText error", { error });
+export const runtime = 'nodejs';
+
+const AGENT_SYSTEM_TEMPLATE = `You are a personal assistant named Assistant0. You are a helpful assistant that can answer questions and help with tasks. You have access to a set of tools, use the tools as needed to answer the user's question. Render the email body as a markdown block, do not wrap it in code blocks.`;
+
+/**
+ * This handler initializes and calls an tool calling agent.
+ */
+export async function POST(req: NextRequest) {
+  const { id, messages }: { id: string; messages: Array<Message>; selectedChatModel: string } = await req.json();
+
+  const tools = {
+    getUserInfo: getUserInfoTool,
+  };
+
+  return createDataStreamResponse({
+    execute: async (dataStream: DataStreamWriter) => {
+      const result = streamText({
+        model: openai('gpt-4o-mini'),
+        system: AGENT_SYSTEM_TEMPLATE,
+        messages,
+        maxSteps: 5,
+        tools,
+      });
+
+      result.mergeIntoDataStream(dataStream, {
+        sendReasoning: true,
+      });
+    },
+    onError: (err: unknown) => {
+      console.log(err);
+      return 'Oops, an error occured!';
     },
   });
-
-  return result.toDataStreamResponse();
 }
--- call-apis-on-users-behalf/your-api-next-js/src/lib/auth0.ts
+++ call-apis-on-users-behalf/your-api-next-js/src/lib/auth0.ts
@@ -1,4 +1,3 @@
-import { Auth0Client } from "@auth0/nextjs-auth0/server";
+import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-// Create an Auth0 Client.
 export const auth0 = new Auth0Client();
