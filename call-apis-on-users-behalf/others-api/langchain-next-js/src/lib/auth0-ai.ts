174f43a3ef009bd8adeaf896ee2fddf8f212cbc9
Deepu K Sasidharan <d4udts@gmail.com>
2025-07-17 22:07:23 +0200

    [sample:langchain] Add others api sample

---
+++ call-apis-on-users-behalf/others-api/langchain-next-js/src/lib/auth0-ai.ts
@@ -0,0 +1,12 @@
+import { Auth0AI, getAccessTokenForConnection } from '@auth0/ai-langchain';
+
+// Get the access token for a connection via Auth0
+export const getAccessToken = async () => getAccessTokenForConnection();
+
+const auth0AI = new Auth0AI();
+
+// Connection for Google services
+export const withGoogleConnection = auth0AI.withTokenForConnection({
+  connection: 'google-oauth2',
+  scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
+});
